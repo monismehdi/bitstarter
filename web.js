40eccc8bb19164081ae1fef4a42aec38a55ac5d5
farazhaider <haider.faraz@outlook.com>
2013-07-08 14:20:56 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs= require('fs');
+
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+
+app.get('/', function(request, response) { 
+ fs.readFile("index.html", function(error, data){}
+  response.send(data);
 });
 
 var port = process.env.PORT || 5000;
